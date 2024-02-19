8cc47bdba269d8ea4c49ff8845bf48d29545552c
Marina Perin <maryfrancisco@live.com>
2024-02-19 16:45:30 +0100

    fix | added vercel link to cors

--- index.js
+++ index.js
@@ -17,7 +17,15 @@ const app = express();
 
 //middlewares
 app.use(morgan("dev"));
-app.use(cors({ origin: ["http://localhost:5173"], credentials: true }));
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://final-project-front-ashy.vercel.app/",
+    ],
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(cookieParser());
 
